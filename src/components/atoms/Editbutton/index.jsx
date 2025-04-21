9aba5f92170e7b5a5879dd86151c9dc071bd89fb
MahiroK <a1r6j37it@outlook.jp>
2025-04-21 22:58:55 +0900

    Atoms・相対パスに変更しました。

--- src/components/atoms/Editbutton/index.jsx
+++ src/components/atoms/Editbutton/index.jsx
@@ -1,6 +1,6 @@
 // EditButton.jsx
 import React from "react";
-import pencilIcon from "src/assets/svg/pencil.svg";
+import pencilIcon from "../../assets/pencil.svg";
 
 export const EditButton = ({ onClick }) => {
   return (
