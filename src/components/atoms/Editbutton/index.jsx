6912b0f477b001bb03de417e3168311d98a3685e
MahiroK <a1r6j37it@outlook.jp>
2025-04-21 22:55:10 +0900

    EditButtonに修正しました

--- src/components/atoms/Editbutton/index.jsx
+++ src/components/atoms/Editbutton/index.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import pencilIcon from "src/assets/svg/pencil.svg";
 
-export const Editbutton = ({ onClick }) => {
+export const EditButton = ({ onClick }) => {
   return (
     <button onClick={onClick}>
       <img src={pencilIcon} alt="Edit" />
