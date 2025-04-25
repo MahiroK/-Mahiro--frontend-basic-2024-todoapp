80b7be4b93d0bdbbd53ce2cdea70be3035623bd3
MahiroK <a1r6j37it@outlook.jp>
2025-04-25 23:09:10 +0900

    ボタンとホバーを作成しました。

---
+++ src/components/atoms/AddTaskButton/index.stories.jsx
@@ -0,0 +1,6 @@
+import { AddTaskButton } from "./index.jsx";
+// ここで呼び出している
+//storybookで確認できるようにしている
+export default { component: AddTaskButton, title: "Atoms/AddTaskButton" };
+
+export const Default = {};
