a0fbcdcbc015a9701abdf4300d74abaa749d2339
MahiroK <a1r6j37it@outlook.jp>
2025-04-25 22:14:02 +0900

    一次レビューを受けて変更しました。

--- src/components/atoms/Editbutton/index.jsx
+++ src/components/atoms/Editbutton/index.jsx
@@ -2,40 +2,27 @@
 import React from "react";
 import pencilIcon from "../../../assets/svg/pencil.svg";
 import styled from "styled-components";
+import COLOR from "../../../variables/color";
 
-const ButtonWrapper = styled.button`
-  position: relative;
-  background: none;
-  border: none;
-  padding: 8px;
-  cursor: pointer;
-
-  &:hover::before {
-    content: "";
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 36px;
-    height: 36px;
-    background-color: rgba(185, 177, 177, 0.1);
-    border-radius: 50%;
-    z-index: 0;
-  }
-`;
-const IconWrapper = styled.span`
-  position: relative;
-  z-index: 1;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-`;
 export const EditButton = ({ onClick }) => {
   return (
     <ButtonWrapper onClick={onClick}>
-      <IconWrapper>
-        <img src={pencilIcon} alt="Edit" />
-      </IconWrapper>
+      <img src={pencilIcon} alt="Edit" />
     </ButtonWrapper>
   );
 };
+
+const ButtonWrapper = styled.button`
+  cursor: pointer;
+  border: none;
+  width: 20px;
+  height: 20px;
+  background-color: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    background-color: ${COLOR.LIGHT_GRAY_TRANSLUCENT};
+    border-radius: 50%;
+  }
+`;
