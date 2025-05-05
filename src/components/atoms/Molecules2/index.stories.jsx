544c1772682943b0313dbb55dfe7957bd403d6f7
MahiroK <a1r6j37it@outlook.jp>
2025-05-05 21:16:45 +0900

    Taskを作成しました。

---
+++ src/components/atoms/Molecules2/index.stories.jsx
@@ -0,0 +1,10 @@
+import { Task } from "./index.jsx";
+export default { component: Task, title: "Molecules/Task" };
+export const Default = {
+  args: {
+    onTaskNameChange: (value) => console.log("Task name changed to:", value),
+    onTaskComplete: () => console.log("Task completed"),
+    taskName: "taskname",
+    defaultIsEditing: false,
+  },
+};
